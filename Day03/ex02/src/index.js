c9ba74b726c65847484bd5da150358db40d013ee
[MirsadZagrljaca] <[mzagrljaca@gmail.com]>
2021-04-03 18:22:13 +0200

    Day 03

---
+++ Day03/ex02/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+  // Only change code below this line
+  //One line comment
+  /*
+    More 
+    Lines 
+    Comment
+    Zero
+    Output
+    */
+  // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
---
+++ Day03/ex05/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  // Only change code below this line
+  var fooBoo = 0;
+  var x = 8;
+  // Only change code above this line
+  return x;
+}
+
+console.log(main());
+
+module.exports = main;
