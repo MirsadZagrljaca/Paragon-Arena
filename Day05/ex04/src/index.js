b5baa8b729268a56f0d8338e1fba5f76c5a90e34
[MirsadZagrljaca] <[mzagrljaca@gmail.com]>
2021-04-03 18:24:01 +0200

    Day 05

---
+++ Day05/ex04/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  // Only change code below this line
+  var firstString = "This is the first string.";
+  var secondString = "This is the second string.";
+  var myString = firstString + secondString;
+  // Only change code above this line
+  return myString;
+}
+
+console.log(main());
+module.exports = main;
