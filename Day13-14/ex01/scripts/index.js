6fdf99aaa4bce8fd1ee346784d6760caeeeeff0a
[MirsadZagrljaca] <[mzagrljaca@gmail.com]>
2021-04-03 18:32:07 +0200

    Day 13-14 2nd Weekend Project

---
+++ Day13-14/ex01/scripts/index.js
@@ -0,0 +1,25 @@
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+function openModal(page) {
+  var modalIframe = document.getElementById("page");
+  modalIframe.src = page;
+  document.getElementById("myModal").style.display = "block";
+}
